7960b97de1708b138fd955d152a888f91028dbbf
Alan Altamirano Hernandez <altamiranohernandezalan@gmail.com>
2025-01-01 13:03:00 -0600

    Mitad

--- src/main/java/altamirano/hernandez/spring_boot_jpa/SpringBootJpaApplication.java
+++ src/main/java/altamirano/hernandez/spring_boot_jpa/SpringBootJpaApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 public class SpringBootJpaApplication implements CommandLineRunner {
@@ -21,9 +22,27 @@ public class SpringBootJpaApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        List<Person> persons = (List<Person>) ipersonRepository.findAll();
+        System.out.println("\n\t *** LISTA DE PERSONS ***");
+        List<Person> persons = (List<Person>) ipersonRepository.buscarPorLenguajedeProgramacion("Java");
         for (var person: persons){
             System.out.println("person = " + person);
         }
+
+        System.out.println("\n\t*** PERSONA EN ESPECIFICO ***");
+        var personaEncontrada = ipersonRepository.buscarPorNombre("Alan");
+        System.out.println("personaEncontrada = " + personaEncontrada);
+
+        System.out.println("\n\t*** BUSQUEDA POR APELLIDOS ***");
+        Person personaEncontradoPorApellidos = ipersonRepository.buscarPorApellidos("Altamirano Hernandez");
+        System.out.println("personaEncontradoPorApellidos = " + personaEncontradoPorApellidos);
+
+        System.out.println("\n\t*** BUSQUEDA POR ID ***");
+        Person personaPorId = ipersonRepository.findById(10).orElse(null);
+        System.out.println("personaPorId = " + personaPorId);
+
+        System.out.println("\n\t*** CREACION (INSERCION) NUEVO OBJETO EN LA DB ***");
+        Person person1 = new Person("Raquel", "Hernandez Hernandez", "Phyton, React, Angular");
+        ipersonRepository.save(person1);
+        System.out.println("Objeto guardado en la db");
     }
 }
--- src/main/java/altamirano/hernandez/spring_boot_jpa/repositories/IPersonRepository.java
+++ src/main/java/altamirano/hernandez/spring_boot_jpa/repositories/IPersonRepository.java
@@ -1,7 +1,25 @@
 package altamirano.hernandez.spring_boot_jpa.repositories;
 
 import altamirano.hernandez.spring_boot_jpa.entities.Person;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface IPersonRepository extends CrudRepository<Person, Integer> {
+    //Definimos metodo personalizado (siguiendo la nomenclatura)
+    List<Person> findByLenguajeDeProgramacion(String lenguajeDeProgramacion);
+
+    //Definimos metodo personalizado que no sigue la nomenclatura
+    @Query("SELECT p FROM Person p WHERE p.lenguajeDeProgramacion =:lenguaje")
+    List<Person> buscarPorLenguajedeProgramacion(@Param("lenguaje") String lenguaje);
+
+    //Consulta Personalizada → Por Nombre
+    @Query("SELECT p FROM Person p WHERE p.nombre =:nombre")
+    Person buscarPorNombre(@Param("nombre") String nombre);
+
+    //Consulta Personalizada → Por Apellidos
+    @Query("SELECT p FROM Person p WHERE p.apellidos =:apellidos")
+    Person buscarPorApellidos(@Param("apellidos") String apellidos);
 }
